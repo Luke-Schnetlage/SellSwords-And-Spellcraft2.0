db3bc261d9cc0dd86c9856115c49fe65bbce683e
Nicolas Towery <nicolatowery@gmail.com>
2023-03-14 13:53:45 -0500

    implemented user authentication
    Need to create a local to get it to work in concordanve with Controller/authentication/index.js and the db file

--- Client/src/Game.js
+++ Client/src/Game.js
@@ -1,6 +1,12 @@
 export default function Game(){
+
+    //if the user is logged in, allow them to play
+    if(localStorage.getItem("user")){
+        console.log(localStorage)
+        return(<h1>Game</h1>)
+    }
     return(
-        <h1>Game</h1>
+        <h1>Please log in to play</h1>
     )
     
 }
